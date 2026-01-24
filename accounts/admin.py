8fec40e42c13f19987f375dce169944ba976cc25
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2026-01-24 10:24:49 -0700

    add Bootstrap styling

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,13 +10,22 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    # To control which fields are listed, we use list_display
     list_display = [
         "email",
         "username",
         "age",
         "is_staff"
     ]
+    # To edit new custom fields, like age, we must add fieldsets
     fieldsets = UserAdmin.fieldsets +  ((None, {"fields": ("age",)}),)
+    # To include a new custom field in the section for creating a new user we rely on add_fieldsets
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+    '''
+    There are many ways to customize the user admin, 
+    and some developers like to add additional options such as 
+    list_filter, search_fields, and ordering.
+    '''
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts"
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "accounts",
+    "pages",
 ]
 
 MIDDLEWARE = [
@@ -125,5 +128,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "pages:home"
+LOGOUT_REDIRECT_URL = "pages:home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
