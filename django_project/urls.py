60259077d4dc11ca8ced1ef2a967ada416d4691d
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2026-02-02 13:11:37 -0700

    newspaper app

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include  # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),  # new
-    path("accounts/", include("django.contrib.auth.urls")),  # new
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("articles/", include("articles.urls")),
     path("", include("pages.urls")),
 ]
